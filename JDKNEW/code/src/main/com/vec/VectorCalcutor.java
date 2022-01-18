1b7bc49a7dcfa8c9a6f168e47dc4107e8cb1b15a
worldluoji <qq1132416@outlook.com>
2022-01-18 17:17:36 +0800

    @Vec@luoji@20220118

---
+++ JDKNEW/code/src/main/com/vec/VectorCalcutor.java
@@ -0,0 +1,37 @@
+package src.main.com.vec;
+
+import src.main.com.returned.Returned;
+
+/** 
+**  孵化期的特性, JDK17目前没有，最新版本JDK才有
+**  求 y = a0x0 + a1x1 + ... + anxn
+**  性能比写for循环快10倍
+**/
+public class VectorCalcutor {
+    public static void main(String[] args) {
+        
+        static final float[] a = new float[] {0.6F, 0.7F, 0.8F, 0.9F};
+        static final FloatVector va =
+                FloatVector.fromArray(FloatVector.SPECIES_128, a, 0);
+                
+        static final float[] x = new float[] {1.0F, 2.0F, 3.0F, 4.0F};
+        static final FloatVector vx =
+                FloatVector.fromArray(FloatVector.SPECIES_128, x, 0);
+    }
+
+    
+    private static Returned<Float> sumInVector(FloatVector va, FloatVector vx) {
+        if (va == null || vx == null || va.length() != vx.length()) {
+            return new Returned.ErrorCode(-1);
+        }
+        
+        // FloatVector vy = va.mul(vx);
+        float[] y = va.mul(vx).toArray();
+        
+        float r = 0F;
+        for (int i = 0; i < y.length; i++) {
+            r += y[i];
+        }
+        return new Returned.ReturnValue<>(r);
+    }
+}
